ba8ffc65b19bb8f64a8bc78c677ea2f1d3446d36
Vikas <vikasedu10@gmail.com>
2022-05-11 22:03:25 +0530

    Use absolute path for mvn in Jenkins container (add to path not working)

--- script.groovy
+++ script.groovy
@@ -1,6 +1,6 @@
 def buildApp() {
     echo "Building the application"
-    sh "mvn -B -DskipTests clean package"
+    sh "/opt/maven/bin/mvn -B -DskipTests clean package"
 }
 
 def buildImage() {
@@ -10,7 +10,7 @@ def buildImage() {
 
 def testApp() {
     echo "Testing app"
-    sh "mvn test"
+    sh "/opt/maven/bin/mvn test"
 }
 
 def deployImage() {
